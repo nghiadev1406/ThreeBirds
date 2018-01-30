c1a26203b6b6e459fca2fb4d32121969fad9f238
Nghia <nghiadev1406@gmail.com>
2018-01-30 09:26:16 +0700

    Modify Login Activity

---
+++ app/src/main/java/com/example/nghia/threebirds/SanPhamActivity.java
@@ -0,0 +1,13 @@
+package com.example.nghia.threebirds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SanPhamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_san_pham);
+    }
+}
---
+++ app/src/main/java/com/example/nghia/threebirds/ThemSanPhamActivity.java
@@ -0,0 +1,13 @@
+package com.example.nghia.threebirds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThemSanPhamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_san_pham);
+    }
+}
